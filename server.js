95c96d4ce8c7136e5f42a30e84cad3097f009e9c
DartRomanius <58496044r@gmail.com>
2016-06-24 01:48:33 +0300

    Rejected commit: Default

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+
+function start(route, handle) {
+   function onRequest(request, response) {
+       var pathName = url.parse(request.url).pathname;
+       console.log("Request for " + pathName + " received");
+
+       route(handle, pathName);
+       
+       response.writeHead(200, {"Content-type": "text/plain"});
+       response.write("Hello World");
+       response.end();
+   }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started");
+}
+
+exports.start = start;
